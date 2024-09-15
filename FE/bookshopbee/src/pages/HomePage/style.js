2e85b979e970a887a3e39e9a2f8022dff6281627
Thai Bao Duy <thaibaoduy.5724@gmail.com>
2024-09-15 15:34:40 +0700

    part9

--- FE/bookshopbee/src/pages/HomePage/style.js
+++ FE/bookshopbee/src/pages/HomePage/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ButtonComponent from "../../component/ButtonComponent/ButtonComponent";
 
 export const WrapperTypeProduct = styled.div`
   display: flex;
@@ -7,4 +8,24 @@ export const WrapperTypeProduct = styled.div`
   justify-content: flex-start;
   border-bottom: 1px solid gray;
   height: 44px;
+`;
+
+export const WrapperButtonMore = styled(ButtonComponent)`
+  &:hover {
+    color: #fff;
+    background: rgb(13, 92, 182);
+    span {
+      color: #fff;
+    }
+  }
+    
+  }
+`;
+
+export const WrapperProduct = styled.div`
+  margin-top: 25px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
 `;
